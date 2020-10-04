b678d9dfe10b298844d5650826b4c8ed42f3ee5a
Eric Wang <ericrwang2011@gmail.com>
2020-10-04 01:21:17 -0500

    added buttons

--- src/components/cards.js
+++ src/components/cards.js
@@ -1,11 +1,11 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
 import { Parallax } from 'react-parallax';
 import food from './pictures/burger.jpg';
 import code from './pictures/coding.jpg';
 import picture from './pictures/picture.jpg';
 
-
-const Cards = props =>{
+export default function Cards() {
     return (
         <div className="cards">
             <div className="card">
@@ -15,11 +15,11 @@ const Cards = props =>{
                 <div className="cardBodyText">
                     <h4 className="cardTitle">Endless Food</h4>
                     <p className="cardInfo">
-                        Living in Austin has be a blessing. There is so much diversity in the types
-                        of food you can find here whether it be Tex-Mex all the way to amazing Asian
-                        cuisines. 
+                        In 2018 I embarked on a journey to eat at the best restaurants here in Austin. 
+                        I've been exploring many unique restaurants downtown Austin near UT campus and have 
+                        compiled a list of my go to spots. Follow me as I eat my way through Austin!
                     </p>
-                    <a href="#" className="btn">Food Blog</a>
+                    <Button className="buttoncard">Foodie</Button>{' '}
                 </div>
             </div>
             <div className="card">
@@ -29,9 +29,11 @@ const Cards = props =>{
                 <div className="cardBodyText">
                     <h4 className="cardTitle">Lines of Code</h4>
                     <p className="cardInfo">
-                        
+                        I started coding in High School first learning Java. Since my introduction into 
+                        coding I have learned Python and C++ in College and JavaScript, Html, and CSS 
+                        alone in my free time. Follow my GitHub to see some of the projects I've done!
                     </p>
-                    <a href="#" className="btn">My Code</a>
+                    <Button className="buttoncard">Coding</Button>{' '}
                 </div>
             </div>
             <div className="card">
@@ -41,13 +43,13 @@ const Cards = props =>{
                 <div className="cardBodyText">
                     <h4 className="cardTitle">Behind the Lens</h4>
                     <p className="cardInfo">
-                         
+                        I picked up photography/film-making after getting my hands on my own Sony RX10 IV.
+                        Watching cinematics on youtube inspired me to start making films and take photos 
+                        to capture special moments throughout my life. View my work here!
                     </p>
-                    <a href="#" className="btn">Explore</a>
+                    <Button className="buttoncard">Explore</Button>{' '}
                 </div>
             </div>
         </div>
     );
 }
-
-export default Cards;
--- src/components/social.js
+++ src/components/social.js
@@ -11,19 +11,19 @@ export default function Social() {
     return (
         <div className="socialContainer">
             <a href="https://www.youtube.com/channel/UCjicEQkOdP7x2kAn120mQFw?view_as=subscriber"
-                className="youtube social">
+                target="_blank" className="youtube social">
                 <FontAwesomeIcon icon={faYoutube} size="2x" />
             </a>
             <a href="https://www.instagram.com/_ericwang_/"
-                className="instagram social">
+                target="_blank" className="instagram social">
                 <FontAwesomeIcon icon={faInstagram} size="2x" />
             </a>
             <a href="https://www.linkedin.com/in/eric-wang-407209169/"
-                className="linkedin social">
+                target="_blank" className="linkedin social">
                 <FontAwesomeIcon icon={faLinkedin} size="2x" />
             </a>
             <a href="https://github.com/Eric-rWang"
-                className="github social">
+                target="_blank" className="github social">
                 <FontAwesomeIcon icon={faGithub} size="2x" />
             </a>
         </div>

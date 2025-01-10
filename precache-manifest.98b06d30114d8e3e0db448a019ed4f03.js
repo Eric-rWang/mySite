f951c92d78dd83e38c85f82e3fc8016dde51c1ab
Eric <ericrwang2011@gmail.com>
2025-01-10 10:58:35 -0600

    Updates

---
+++ precache-manifest.98b06d30114d8e3e0db448a019ed4f03.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "320f846862cb7edcbec53ce1bf12ace7",
+    "url": "/mySite/index.html"
+  },
+  {
+    "revision": "077ae8cf2358554d52ef",
+    "url": "/mySite/static/css/main.b0e035fb.chunk.css"
+  },
+  {
+    "revision": "19ab24ab7873da976833",
+    "url": "/mySite/static/js/2.597054ec.chunk.js"
+  },
+  {
+    "revision": "6fea1d5f501b2a38a20e0c39126299c8",
+    "url": "/mySite/static/js/2.597054ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "077ae8cf2358554d52ef",
+    "url": "/mySite/static/js/main.ca144f60.chunk.js"
+  },
+  {
+    "revision": "e760ff840bffb924785b",
+    "url": "/mySite/static/js/runtime-main.e5b54dcb.js"
+  },
+  {
+    "revision": "b770c67d9b46be3bceb413befeb8735d",
+    "url": "/mySite/static/media/Japan2023.b770c67d.mp4"
+  },
+  {
+    "revision": "2118938e7b38d542f3d19357b9b5bc7c",
+    "url": "/mySite/static/media/background.2118938e.mp4"
+  },
+  {
+    "revision": "5078b16513ec8dfcf2fa4ca7e7698921",
+    "url": "/mySite/static/media/bluebird.5078b165.jpg"
+  },
+  {
+    "revision": "3e0e435e33ea8f9a692bde6c6aca655c",
+    "url": "/mySite/static/media/burger.3e0e435e.jpg"
+  },
+  {
+    "revision": "e69d83e8572587476df610b85ab23887",
+    "url": "/mySite/static/media/coding.e69d83e8.jpg"
+  },
+  {
+    "revision": "31e956a7f4ff1bfb0b8607cf15b14530",
+    "url": "/mySite/static/media/cooking_background.31e956a7.mp4"
+  },
+  {
+    "revision": "d85c4b3d003d6ac7d02da2248c11776a",
+    "url": "/mySite/static/media/eem.d85c4b3d.jpg"
+  },
+  {
+    "revision": "fae0fe02fbedde82cb6ded8a800de4c2",
+    "url": "/mySite/static/media/fooooood.fae0fe02.mp4"
+  },
+  {
+    "revision": "d9c3308ee62c7156874167cd4cadeff3",
+    "url": "/mySite/static/media/headshotnew.d9c3308e.png"
+  },
+  {
+    "revision": "552e5e2be87b471234992fcd0fd39c7a",
+    "url": "/mySite/static/media/kochi.552e5e2b.jpg"
+  },
+  {
+    "revision": "c3cef6e718592e7782c76a22389713dd",
+    "url": "/mySite/static/media/lee.c3cef6e7.jpg"
+  },
+  {
+    "revision": "493621076f2e49958a8b477522125cfc",
+    "url": "/mySite/static/media/loro.49362107.jpg"
+  },
+  {
+    "revision": "dfc26ba2ef1ba8d0e45fb2f82738ddf8",
+    "url": "/mySite/static/media/nongs.dfc26ba2.jpg"
+  },
+  {
+    "revision": "37ed2b957d34309e23ca7313856bb33a",
+    "url": "/mySite/static/media/phateatery.37ed2b95.jpg"
+  },
+  {
+    "revision": "f865ac176737d0f53411eb8fd4840d4a",
+    "url": "/mySite/static/media/phooregon.f865ac17.jpg"
+  },
+  {
+    "revision": "bb0f442f5475464002b12fec23f4e42f",
+    "url": "/mySite/static/media/photography.bb0f442f.JPG"
+  },
+  {
+    "revision": "0f45ba4e930af85422befb062c802c54",
+    "url": "/mySite/static/media/project1.0f45ba4e.jpg"
+  },
+  {
+    "revision": "62aaea4eabc8f3fd2317422c78f903a4",
+    "url": "/mySite/static/media/project2.62aaea4e.jpg"
+  },
+  {
+    "revision": "1b3ecb58dc3cc9abab396ed6e88a14b0",
+    "url": "/mySite/static/media/project3.1b3ecb58.jpg"
+  },
+  {
+    "revision": "25bea9af170864c0eeb9906f484ed397",
+    "url": "/mySite/static/media/semma.25bea9af.jpg"
+  },
+  {
+    "revision": "6bc19c3e03662a020626f72e4e028083",
+    "url": "/mySite/static/media/typing.6bc19c3e.mp4"
+  }
+]);
